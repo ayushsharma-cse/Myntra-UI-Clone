95be9bbe2d2987addd67204fca7650ac5209c576
ayushsharma-cse <ayushsharma.cse79>
2025-12-09 22:40:33 +0530

    redux implemented

---
+++ src/Redux/Slice/product_slice.jsx
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { MyntraData } from "../../data/index";
+
+const initialState = {
+  products: MyntraData.products,
+  orders: [],
+  cart: [],
+};
+
+export const productSlice = createSlice({
+  name: "product",
+  initialState,
+  reducers: {
+    setProducts: (state, action) => {
+      state.products = action.payload;
+    },
+    setOrders: (state, action) => {
+      state.orders = action.payload;
+    },
+    setCart: (state, action) => {
+      state.cart = action.payload;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { setProducts, setOrders, setCart } = productSlice.actions;
+
+export default productSlice.reducer;
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./Redux/store.jsx";
+import store from "./Redux/store.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
